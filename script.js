b4001b2edce78a2490ba78cd60f4d33c605c431f
Chad Harper <chadharper811@gmail.com>
2024-11-16 18:45:22 -0600

    updated projects section with javascript to populate the projects

--- script.js
+++ script.js
@@ -1,9 +1,81 @@
 const data = [
     {
+        href: "https://chadharper811.github.io/Resume/",
+        imgLink: "Pictures/Resume.png",
         title: "Resume Website",
         date: "Jan 28, 2023",
-        link: "https://chadharper811.github.io/Resume/",
-        imgLink: "Pictures/Resume.png"
+    },
+    {
+        href: "https://chadharper811.github.io/panthera/",
+        imgLink: "Pictures/Nike.png",
+        title: "Nike Panthera Series Website",
+        date: "Apr 17, 2023"
+    },
+    {
+        href: "https://chadharper811.github.io/Harpers-Adventures/",
+        imgLink: "Pictures/Harper's Adventures.png",
+        title: "Harper's Adventures Travel Blog",
+        date: "Jul 27, 2023"
+    },
+    {
+        href: "https://chadharper811.github.io/Tribute-page/",
+        imgLink: "Pictures/Tribute Page.png",
+        title: "Steven Hawking Tribute Page",
+        date: "Nov 26, 2023"
+    },
+    {
+        href: "https://chadharper811.github.io/Customer-Service-Survey/",
+        imgLink: "Pictures/Survey.png",
+        title: "Customer Service Survey",
+        date: "Nov 27, 2023"
+    },
+    {
+        href: "https://github.com/ChadHarper811/BookMaster",
+        imgLink: "Pictures/BookMaster.png",
+        title: "BookMaster React App",
+        date: "Jan 22, 2024"
+    },
+    {
+        href: "https://chadharper811.github.io/Technical-Page/",
+        imgLink: "Pictures/King of Tokyo Rulebook.png",
+        title: "King of Tokyo Rulebook",
+        date: "Feb 4, 2024"
+    },
+    {
+        href: "https://chadharper811.github.io/Palidrome-Checker/",
+        imgLink: "Pictures/palindrome checker.png",
+        title: "Palindrome Checker",
+        date: "May 16, 2024"
+    },
+    {
+        href: "https://chadharper811.github.io/Harpers_Adventures_JS/",
+        imgLink: "Pictures/Harper's Adventures JS.png",
+        title: "Harper's Adventures JS Update",
+        date: "Aug 12, 2024"
+    },
+    {
+        href: "https://chadharper811.github.io/roman_numeral_converter/",
+        imgLink: "Pictures/Roman Numeral Converter.png",
+        title: "Roman Numeral Converter",
+        date: "Aug 24, 2024"
+    },
+    {
+        href: "https://chadharper811.github.io/telephone_number_validator/",
+        imgLink: "Pictures/Telephone Number Validator.png",
+        title: "Telephone Number Validator",
+        date: "Sep 2, 2024"
+    },
+    {
+        href: "https://chadharper811.github.io/Updated-Resume/",
+        imgLink: "Pictures/Updated resume Website.png",
+        title: "Updated Resume Website",
+        date: "Oct 20, 2024"
+    },
+    {
+        href: "https://chadharper811.github.io/cash_register/",
+        imgLink: "Pictures/Cash Register.png",
+        title: "Cash Register",
+        date: "Nov 16, 2024"
     },
     {
         text: "Email",
@@ -37,8 +109,17 @@ const data = [
 const contactLinks = document.getElementById("contactLinks");
 const projectsSection = document.getElementById("projectsSection");
 
-
-
+data.filter(el => el.href).forEach(
+    ({href, imgLink, title, date}) => {
+        projectsSection.innerHTML += `
+        <a href="${href}" target="_blank" class="project-tile">
+            <img class="projectImg" src="${imgLink}" alt="${title}"/>
+            <h3>${title}</h3>
+            <p>Completed: ${date}</p>
+        </a>
+        `
+    }
+)
 
 data.filter(el => el.icon).forEach(
     ({id, icon, link, text}) => {
